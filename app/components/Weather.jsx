77c99647ba3818ba3070d648f99fe1319324a34c
Michael Claus <mclausaudio@gmail.com>
2018-05-09 18:51:10 -0700

    added error modal

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -1,7 +1,9 @@
 var React = require('react');
 var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
+var ErrorModal = require('ErrorModal');
 var openWeatherMap = require('openWeatherMap');
+
 
 var Weather = React.createClass({
   getInitialState: function () {
@@ -12,7 +14,10 @@ var Weather = React.createClass({
   handleSearch: function(location) {
     var that = this;
 
-    this.setState({isLoading: true});
+    this.setState({
+      isLoading: true,
+      errorMessage: undefined
+    });
 
     openWeatherMap.getTemp(location).then(function(temp) {
       that.setState({
@@ -20,23 +25,32 @@ var Weather = React.createClass({
         temp: temp,
         isLoading: false
       });
-    }, function (errorMessage) {
-      that.setState({isLoading: false});
-      alert(errorMessage);
-
+    }, function (e) {
+      that.setState({
+        isLoading: false,
+        errorMessage: e.message
+      });
     });
   },
   render: function(){
     // pass in variables from state object
-    var {isLoading, temp, location} = this.state;
+    var {isLoading, temp, location, errorMessage} = this.state;
 
     function renderMessage() {
       if (isLoading) {
-          return <h3 className="text-centered">Fetching weather...</h3>;
+          return <h3 className="text-center">Fetching weather...</h3>;
       } else if (temp && location) {
           return <WeatherMessage temp={temp} location={location}/>;
       }
     };
+
+    function renderError() {
+      if (typeof errorMessage === 'string') {
+        return (
+          <ErrorModal message={errorMessage}/>
+        )
+      }
+    }
 
     return (
       <div>
@@ -45,6 +59,7 @@ var Weather = React.createClass({
         <WeatherForm onSearch={this.handleSearch}/>
         {/* pass in variables to component by adding them as properties */}
         {renderMessage()}
+        {renderError()}
       </div>
     )
   }
